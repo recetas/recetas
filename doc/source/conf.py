9968f3c7437978868a432e15fdd19ce906d86174
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2016-11-03 16:46:00 +0100

    ENH: include rtd style directly

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -116,9 +116,7 @@ todo_include_todos = True
 import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
+html_theme_path = ['_themes']
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
